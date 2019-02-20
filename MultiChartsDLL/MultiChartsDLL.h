a29bb420fb32795f377d41895db8f8954b0d94a4
Kartik Madan <magiciankartik00@gmail.com>
2019-02-20 17:39:10 +0530

    get-double

--- MultiChartsDLL/MultiChartsDLL.h
+++ MultiChartsDLL/MultiChartsDLL.h
@@ -71,29 +71,22 @@ extern "C" class MULTICHARTSMACRO MultiCharts
 };
 
 extern "C" MultiCharts* CreateMultiCharts();
-
 extern "C" void DisposeMultiCharts(MultiCharts* multiCharts);
 
 extern "C" void SetIntNumber(MultiCharts* multiCharts, int intNumber);
-
 extern "C" int GetIntNumber(MultiCharts* multiCharts);
 
+extern "C" double GetDoubleNumber(MultiCharts* multiCharts);
 extern "C" void SetDoubleNumber(MultiCharts* multiCharts, double doubleNumber);
 
 extern "C" void SetStringData(MultiCharts* multiCharts, char* stringData);
-
 extern "C" char* GetStringData(MultiCharts* multiCharts);
-
 extern "C" void InitStringData(MultiCharts* multiCharts, int size);
 
 extern "C" void SetDoubleArray(MultiCharts* multiCharts, double* doubleArray);
-
 extern "C" double* GetDoubleArray(MultiCharts* multiCharts);
-
 extern "C" void InitDoubleArray(MultiCharts* multiCharts, int size);
 
 extern "C" void InitPythonStringData(MultiCharts* multiCharts, int size);
-
 extern "C" void SetPythonStringData(MultiCharts* multiCharts, char* pythonStringData);
-
 extern "C" char* GetPythonStringData(MultiCharts* multiCharts);
