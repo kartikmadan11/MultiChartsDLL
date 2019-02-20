336981f13b1509726c86cc7397c0ec9967dbcb44
Kartik Madan <magiciankartik00@gmail.com>
2019-02-20 16:36:06 +0530

    added-external-c-functions

--- MultiChartsDLL/MultiChartsDLL.h
+++ MultiChartsDLL/MultiChartsDLL.h
@@ -24,7 +24,7 @@ extern "C" class MULTICHARTSMACRO MultiCharts
 	
 	double learningRate;
 	int epochs;
-	long scale;
+	int scale;
 
 	public:
 
@@ -43,9 +43,30 @@ extern "C" class MULTICHARTSMACRO MultiCharts
 		void InitFileName(int size);
 		void SetFileName(char* fileName);
 
+		void SetLearningRate(double learningRate);
+		void SetEpochs(int epochs);
+		void SetScale(int scale);
 };
+
+/*
+		-- Exported Functions
+*/
 
 extern "C" MultiCharts* CreateMultiCharts();
 extern "C" void DisposeMultiCharts(MultiCharts* multiCharts);
 
-extern "C" void SetTrainingData(MultiCharts* multicharts, double* trainingData);
+extern "C" void InitTrainingData(MultiCharts* multiCharts, int size);
+extern "C" void SetTrainingData(MultiCharts* multiCharts, double* trainingData);
+
+extern "C" void InitDateArray(MultiCharts* multiCharts, int size);
+extern "C" void SetDateArray(MultiCharts* multiCharts, char (*dateArray)[DATE_SIZE]);
+
+extern "C" void InitVolumeArray(MultiCharts* multiCharts, int size);
+extern "C" void SetVolumeArray(MultiCharts* multiCharts, long* volumeArray);
+
+extern "C" void InitFileName(MultiCharts* multiCharts, int size);
+extern "C" void SetFileName(MultiCharts* multiCharts, char* fileName);
+
+extern "C" void SetLearningRate(MultiCharts* multiCharts, double learningRate);
+extern "C" void SetEpochs(MultiCharts* multiCharts, int epochs);
+extern "C" void SetScale(MultiCharts* multiCharts, int scale);
