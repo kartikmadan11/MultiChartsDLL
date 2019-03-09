fb1a20d0f0d583a8a04ee7848de361bbce7c584a
Kartik Madan <magiciankartik00@gmail.com>
2019-03-09 12:27:50 +0530

    minor-changes-to-cpp

--- MultiChartsDLL/MultiChartsDLL.cpp
+++ MultiChartsDLL/MultiChartsDLL.cpp
@@ -115,8 +115,13 @@ void MultiCharts::SetOptimizer(short optimizer)
 
 double MultiCharts::TrainModel()
 {
-	/*
+	
 	CPyInstance pyInstance;
+	PyThreadState *_save; 
+	_save = PyEval_SaveThread();
+	PyRun_SimpleString("import numpy as np");
+	PyEval_RestoreThread(_save);
+	/*
 	CPyObject pModule = PyImport_ImportModule("build");
 
 	if (pModule)
