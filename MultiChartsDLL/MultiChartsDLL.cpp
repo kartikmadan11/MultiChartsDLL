db5c5253c79bbe347edd8436801cb5edd0fa8ca5
Kartik Madan <magiciankartik00@gmail.com>
2019-03-14 17:52:31 +0530

    added null check to TrainModel()

--- MultiChartsDLL/MultiChartsDLL.cpp
+++ MultiChartsDLL/MultiChartsDLL.cpp
@@ -234,5 +234,8 @@ void SetOptimizer(MultiCharts* multiCharts, short optimizer)
 
 double TrainModel(MultiCharts* multiCharts)
 {
-	return multiCharts->TrainModel();
+	if (multiCharts != NULL)
+	{
+		return multiCharts->TrainModel();
+	}
 }
